a532f18d68053293eb648acddec136a3fa8348a5
AlanAndrzejSkowronek <alanskowronek1414@gmail.com>
2022-04-01 19:19:41 +0200

    Creación del tablero y testeos.

--- src/IniciarJuego.java
+++ src/IniciarJuego.java
@@ -1,6 +1,9 @@
 public class IniciarJuego {
 
     public static void main(String[] args){
+        Tablero t = new Tablero(7, 7);
 
+        t.initTablero();
+        t.printTablero();
     }
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -1,2 +1,33 @@
 public class Tablero {
+    private String[][] tablero;
+    private int altura, longitud;
+
+    public Tablero(int altura, int longitud){
+        this.altura = altura;
+        this.longitud = longitud;
+        tablero = new String[altura][longitud];
+    }
+
+    public int getAltura(){ return altura; }
+    public int getLongitud(){ return longitud; }
+
+    public void initTablero(){
+
+        for (int i = 0; i < tablero.length; i++){
+            for(int j = 0; j < tablero[i].length; j++){
+                tablero[i][j] = "_";
+            }
+        }
+    }
+
+    public void printTablero(){
+
+        for (int i = 0; i < tablero.length; i++){
+            System.out.print(" | ");
+            for(int j = 0; j < tablero[i].length; j++){
+                System.out.print(tablero[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
 }
