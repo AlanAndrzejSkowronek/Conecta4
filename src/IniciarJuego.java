a0d1aa9b81363f7f3e8c677218a8d495ae3a2d92
AlanAndrzejSkowronek <alanskowronek1414@gmail.com>
2022-04-01 20:24:06 +0200

    Elección de la casilla por input.

--- src/IniciarJuego.java
+++ src/IniciarJuego.java
@@ -1,9 +1,12 @@
 public class IniciarJuego {
+    public static Tablero t = new Tablero(7, 7);
+
 
     public static void main(String[] args){
-        Tablero t = new Tablero(7, 7);
+        InputUsuario inpus = new InputUsuario();
 
         t.initTablero();
         t.printTablero();
+        inpus.EleccionCasilla();
     }
 }
--- src/InputUsuario.java
+++ src/InputUsuario.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class InputUsuario {
+    private Scanner leer = new Scanner(System.in);
+    private Tablero t = IniciarJuego.t;
+    private int numMinimo = 1;
+
+    public int EleccionCasilla(){
+        int numElegido = 0;
+
+        do {
+            numElegido = leer.nextInt();
+            if (numElegido > t.getAltura() || numElegido < numMinimo){
+                System.out.println("No existe esa columna. Porfavor introduzca una en el rango: " + "[" + numMinimo + " - " + t.getAltura() + "]");
+            }
+        } while (numElegido > t.getAltura() || numElegido < numMinimo);
+
+        return numElegido;
+    }
+
 }
